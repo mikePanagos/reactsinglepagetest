ce016c8b16bb3c7274f5dae61a5cfadcf73382b7
Michael Panagos <michael@technicool.com>
2019-05-30 14:36:42 -0500

    added actions for post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 // import axios from 'axios';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import {deletePost } from '../actions/postActions'
 
 class Post extends Component {
     // state = {
@@ -16,11 +17,20 @@ class Post extends Component {
     //         });
 
     // }
+    handleClick=()=>{
+        this.props.deletePost(this.props.post.id);
+        this.props.history.push('/')
+    }
     render() {
+        console.log(this.props);
+        
         const post = this.props.post ? (
             <div className="post">
                 <h4 className="center">{this.props.post.title}</h4>
                 <p>{this.props.post.body}</p>
+                <div className="center">
+                    <button className="btn gray" onClick={this.handleClick}>delete</button>
+                </div>
             </div>
         ) : (
                 <div className="center">loading post</div>
@@ -32,13 +42,17 @@ class Post extends Component {
         )
     }
 }
-const mapStateToProps=(state,ownProps)=>{
+const mapStateToProps = (state, ownProps) => {
     console.log(ownProps);
-    
-    let id= ownProps.match.params.post_id;
-    return {
-        post:state.posts.find(post=>post.id===id)
-    }
 
+    let id = ownProps.match.params.post_id;
+    return {
+        post: state.posts.find(post => post.id === id)
+    }
 }
-export default connect(mapStateToProps)(Post);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deletePost:(id)=>{dispatch(deletePost(id))}
+    }
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Post);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -4,15 +4,41 @@
 
 
 
-const initState={
-    posts:[
-        {userId: '1', id: "1", title: "Squirtle Laid an Egg", body: "quia et suscipit↵suscipit recusandae consequuntur …strum rerum est autem sunt rem eveniet architecto"},
-        {userId: '1', id: '2', title: "Charmander Laid an Egg", body: "est rerum tempore vitae sequi sint nihil reprehend…aperiam non debitis possimus qui neque nisi nulla"},
-        {userId: '1', id: '3', title: "a Helix Fossil was Found", body: "et iusto sed quo iure↵voluptatem occaecati omnis e…↵molestiae porro eius odio et labore et velit aut"}
-    ]
+const initState = {
+    posts: [
+        { userId: '1', id: "1", title: "Squirtle Laid an Egg", body: "quia et suscipit↵suscipit recusandae consequuntur …strum rerum est autem sunt rem eveniet architecto" },
+        { userId: '1', id: '2', title: "Charmander Laid an Egg", body: "est rerum tempore vitae sequi sint nihil reprehend…aperiam non debitis possimus qui neque nisi nulla" },
+        { userId: '1', id: '3', title: "a Helix Fossil was Found", body: "et iusto sed quo iure↵voluptatem occaecati omnis e…↵molestiae porro eius odio et labore et velit aut" }
+    ],
+    commentsOnpage:
+        [
+            {
+                name: "home",
+                comments: []
+            },
+            {
+                name: "about",
+                comments: []
+            },
+            {
+                name: "contact",
+                comments: []
+            }
+        ]
+
+
 }
 
-const rootReducer=((state=initState,action)=>{
+const rootReducer = ((state = initState, action) => {
+    if(action.type==='DELETE_POST'){
+        let newPosts=state.posts.filter(post=>{
+            return action.id!==post.id;
+        });
+        return{
+            ...state,
+            posts:newPosts
+        }
+    }
     return state;
 })
 export default rootReducer;
